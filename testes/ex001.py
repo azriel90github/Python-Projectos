d5b82ab0542cbce8c24930a34240fd3749740b47
azriel90github <azrielgithub@gmail.com>
2024-03-06 20:42:23 +0000

    introdução

---
+++ testes/ex001.py
@@ -0,0 +1,13 @@
+print  ('Olá Mundo') # Mostrar essa mesma mensagem na tela
+
+nomeCompleto = "Azriel Suélio Moreira Armando!"
+nomeMaiuscula = nomeCompleto.upper()
+nomeMenuscula = nomeCompleto.lower()
+
+
+
+print(nomeMaiuscula)
+print(nomeMenuscula)
+
+
+
