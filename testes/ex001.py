967bb00555b30d5deb7907b90eebacbfcef6526b
azriel90github <azrielgithub@gmail.com>
2024-03-07 09:56:29 +0000

    testes

--- testes/ex001.py
+++ testes/ex001.py
@@ -1,13 +1,15 @@
 print  ('Olá Mundo') # Mostrar essa mesma mensagem na tela
 
 nomeCompleto = "Azriel Suélio Moreira Armando!"
-nomeMaiuscula = nomeCompleto.upper()
-nomeMenuscula = nomeCompleto.lower()
+
+
+nomeMaiuscula = nomeCompleto.upper() # Transforar nome completo em maiuscula
+nomeMenuscula = nomeCompleto.lower() # Transforar nome completo em menusula
 
 
 
-print(nomeMaiuscula)
-print(nomeMenuscula)
+print(nomeMaiuscula) # mostrar na tela
+print(nomeMenuscula) # mostrar na tela
 
 
 
